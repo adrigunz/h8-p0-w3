caefd2c304839102e5ed7a0f05e752c31c1242b2
Adrian Adiguna Soeranto <adrigunz@gmail.com>
2019-08-29 16:21:16 +0700

    editing exercise 3 and 4
    adding function dataHandling to exercise 3

--- exercise-3.js
+++ exercise-3.js
@@ -6,20 +6,24 @@ var input = [
     ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
 ]
 
-for (i = 0; i < input.length; i++) { // i is outer array
-    for (j = 0; j < input[i].length; j++) { // j is inner array
-        var kota;
-        if (j === 0) {
-            console.log(`Nomor ID: ${input[i][0]}`);
-        } else if (j === 1) {
-            console.log(`Nama Lengkap: ${input[i][1]}`);
-        } else if (j === 2) {
-            kota = input[i][2];
-        } else if (j === 3) {
-            console.log(`TTL: ${kota} ${input[i][3]}`);
-        } else if (j === 4) {
-            console.log(`Hobi: ${input[i][4]}`);
-            console.log(' ');
+function dataHandling(data) {
+    for (i = 0; i < data.length; i++) { // i is outer array
+        for (j = 0; j < data[i].length; j++) { // j is inner array
+            var kota;
+            if (j === 0) {
+                console.log(`Nomor ID: ${data[i][0]}`);
+            } else if (j === 1) {
+                console.log(`Nama Lengkap: ${data[i][1]}`);
+            } else if (j === 2) {
+                kota = input[i][2];
+            } else if (j === 3) {
+                console.log(`TTL: ${kota} ${data[i][3]}`);
+            } else if (j === 4) {
+                console.log(`Hobi: ${data[i][4]}`);
+                console.log(' ');
+            }
         }
     }
 }
+
+dataHandling(input);
